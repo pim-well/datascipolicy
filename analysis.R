de52279ef5f23a8b25b0a2d4f5605b2014dd3be0
pim-well <124284657+pim-well@users.noreply.github.com>
2024-03-18 13:08:34 -0400

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -3,13 +3,16 @@ library(tidyverse)
 df <- read_csv('data/data_302.csv')
 head(df)
 
+df$AGE_BUCKET <- factor(df$AGE_BUCKET, levels=c("<18", "18-34", "34-49", "50-64", ">64"))
+df$INCARCERATED_BEFORE <- factor(df$INCARCERATED_BEFORE)
+
 # WHAT IS GOING ON?
 df %>%
   group_by(INCARCERATED_BEFORE) %>%
-  summarize(mean(all_death))
+  summarize(mean_death = mean(all_death), n=n())
 
 # OH WAIT MAYBE IT'S AGE
 df %>%
   group_by(AGE_BUCKET, INCARCERATED_BEFORE) %>%
-  summarize(mean(all_death), n()) %>%
+  summarize(mean_death = mean(all_death), n=n()) %>%
   arrange(AGE_BUCKET, INCARCERATED_BEFORE)
